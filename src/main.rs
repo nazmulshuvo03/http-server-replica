a9a0330de3170023d04bfd1d00bbfe0f14c68e1a
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-03-10 11:46:33 +0600

    stage 3: respond with 404

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,16 +14,36 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                let status_line = "HTTP/1.1 200 OK";
-                let crlf = "\r\n";
-                let response = format!("{status_line}{crlf}{crlf}");
-                stream.write_all(response.as_bytes()).unwrap();
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|res| res.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let request_items: Vec<&str> = request_line.split_whitespace().collect();
+    let method = request_items[0];
+    let path = request_items[1];
+    let version = request_items[2];
+    println!("Method: {}, path: {}, version: {}", method, path, version);
+
+    let status_line = match path {
+        "/" => "HTTP/1.1 200 OK",
+        _ => "HTTP/1.1 404 NOT FOUND",
+    };
+    let crlf = "\r\n";
+    let response = format!("{status_line}{crlf}{crlf}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
