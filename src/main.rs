e9ff7fe923c701768c8309a859c2b6f025dc7ecd
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-03-10 13:51:17 +0600

    stage 4: respond with content

--- src/main.rs
+++ src/main.rs
@@ -34,16 +34,57 @@ fn handle_connection(mut stream: TcpStream) {
     //     .collect();
     let request_line = buf_reader.lines().next().unwrap().unwrap();
     let request_items: Vec<&str> = request_line.split_whitespace().collect();
+
     let method = request_items[0];
     let path = request_items[1];
     let version = request_items[2];
     println!("Method: {}, path: {}, version: {}", method, path, version);
 
-    let status_line = match path {
-        "/" => "HTTP/1.1 200 OK",
-        _ => "HTTP/1.1 404 NOT FOUND",
+    let path_params: Vec<&str> = path.split('/').collect();
+    println!("Path params: {:?}", path_params);
+
+    let code = match path_params[1] {
+        "" | "echo" => "200 OK",
+        _ => "404 NOT FOUND",
     };
-    let crlf = "\r\n";
-    let response = format!("{status_line}{crlf}{crlf}");
+    println!("Status code: {}", code);
+
+    let content = if path_params[1] == "echo" {
+        let params = path_params.clone();
+        echo_response(params)
+    } else {
+        "".to_string()
+    };
+    println!("Content: {}", content);
+
+    let response = response_string(code, version, content);
     stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn response_string(code: &str, version: &str, content: String) -> String {
+    let crlf = "\r\n";
+    let content_type = "text/plain";
+    let formatted = format!(
+        "{} {}{}Content-Type: {}{}Content-Length: {}{}{}{}",
+        version,
+        code,
+        crlf,
+        content_type,
+        crlf,
+        content.len(),
+        crlf,
+        crlf,
+        content
+    );
+    println!("Formatted response: {:?}", formatted);
+    formatted
+}
+
+fn echo_response(mut params: Vec<&str>) -> String {
+    if params.len() > 2 {
+        params.drain(0..2);
+    }
+    let result: String = params.join("/");
+    println!("Echo response: {}", result);
+    result
 }
