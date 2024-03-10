98b8cc20a06a1388f9b926a0f16ec0861d671570
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-03-10 16:25:07 +0600

    stage 5: parse headers

--- src/main.rs
+++ src/main.rs
@@ -32,28 +32,38 @@ fn handle_connection(mut stream: TcpStream) {
     //     .map(|res| res.unwrap())
     //     .take_while(|line| !line.is_empty())
     //     .collect();
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let mut buf_reader_lines = buf_reader.lines();
+    let request_line = buf_reader_lines.next().unwrap().unwrap();
+    let _host_line = buf_reader_lines.next().unwrap().unwrap();
+    let user_agent_line = buf_reader_lines.next().unwrap().unwrap();
+
     let request_items: Vec<&str> = request_line.split_whitespace().collect();
+    let user_agent_items: Vec<&str> = user_agent_line.split(": ").collect();
 
     let method = request_items[0];
     let path = request_items[1];
     let version = request_items[2];
     println!("Method: {}, path: {}, version: {}", method, path, version);
 
+    let user_agent = user_agent_items[1];
+    println!("User agent: {}", user_agent);
+
     let path_params: Vec<&str> = path.split('/').collect();
     println!("Path params: {:?}", path_params);
 
     let code = match path_params[1] {
-        "" | "echo" => "200 OK",
+        "" | "echo" | "user-agent" => "200 OK",
         _ => "404 NOT FOUND",
     };
     println!("Status code: {}", code);
 
-    let content = if path_params[1] == "echo" {
-        let params = path_params.clone();
-        echo_response(params)
-    } else {
-        "".to_string()
+    let content = match path_params[1] {
+        "echo" => {
+            let params = path_params.clone();
+            echo_response(params)
+        }
+        "user-agent" => user_agent.to_string(),
+        _ => "".to_string(),
     };
     println!("Content: {}", content);
 
